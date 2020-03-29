face192f19fd216468c562d2298f6dd5c8a5a046
robynp108 <robynp108@gmail.com>
2020-03-28 19:44:07 -0500

    ajax calls for current day plus UV value

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$("#search-button").on("click", function(event) {
+$("#search-button").on("click", function (event) {
     event.preventDefault();
 
     var inputEl = $("#input-city");
@@ -13,7 +13,45 @@ $("#search-button").on("click", function(event) {
     searchHistBtn.text(prevSearch);
     $("#search-history").prepend(searchHistBtn);
 
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&APPID=6271303875633c1ede72b0a41c1ebb3c";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+
+            console.log(queryURL);
+            console.log(response);
+
+            $(".city").html("<h1>" + response.name + "</h1>");
+
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            $(".temp").text("Temperature: " + tempF.toFixed(2) + " F");
+
+            $(".wind").text("Wind Speed: " + response.wind.speed + "MPH");
+            $(".humidity").text("Humidity: " + response.main.humidity + "%");
+
+            var inputLat = response.coord.lat;
+            var inputLon = response.coord.lon;
+            console.log(inputLat);
+            console.log(inputLon);
+
+            var UVqueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=6271303875633c1ede72b0a41c1ebb3c&lat=" + inputLat + "&lon=" + inputLon;
+
+            $.ajax({
+                url: UVqueryURL,
+                method: "GET"
+            })
+                .then(function (UVresponse) {
+                    console.log(UVresponse);
+
+                    $("#uv-index").text(UVresponse.value);
+                });
+
+        });
+
+    var forecastQueryURL = "api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=6271303875633c1ede72b0a41c1ebb3c";
 
 
-    
-  });
+});
