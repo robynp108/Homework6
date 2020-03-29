a55e8ba866eca4e0ebab9b301629e929b36e1c08
robynp108 <robynp108@gmail.com>
2020-03-29 18:31:04 -0500

    final testing, debug, code cleanup, validate html

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ $("#search-button").on("click", function (event) {
 
     var inputEl = $("#input-city");
     var input = inputEl.val();
-    console.log(input);
 
     localStorage.setItem("input", input);
 
     var prevSearch = localStorage.getItem("input");
-    console.log(prevSearch);
 
     var searchHistBtn = $("<button>");
     searchHistBtn.addClass("searchBtns");
@@ -29,13 +27,9 @@ $("#search-button").on("click", function (event) {
         })
             .then(function (response) {
 
-                console.log(queryURL);
-                console.log(response);
-
                 var iconCode = response.weather[0].icon;
                 var iconTodayURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
 
-                //$(".city").html("<h2>" + response.name + ": " + date + "</h2>");
                 $("#city").text(response.name + ": " + date);
                 $("#icon").attr("src", iconTodayURL);
 
@@ -47,8 +41,6 @@ $("#search-button").on("click", function (event) {
 
                 var inputLat = response.coord.lat;
                 var inputLon = response.coord.lon;
-                console.log(inputLat);
-                console.log(inputLon);
 
                 var UVqueryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=6271303875633c1ede72b0a41c1ebb3c&lat=" + inputLat + "&lon=" + inputLon;
 
@@ -60,6 +52,7 @@ $("#search-button").on("click", function (event) {
                         console.log(UVresponse);
 
                         $("#uv-index").text(UVresponse.value);
+                        $("#uv-index").removeAttr("class");
 
                         if (UVresponse.value < 3) {
                             $("#uv-index").addClass("very-low");
@@ -81,7 +74,7 @@ $("#search-button").on("click", function (event) {
         var date3 = moment().add(3, "day").format("MM/DD/YYYY");
         var date4 = moment().add(4, "day").format("MM/DD/YYYY");
         var date5 = moment().add(5, "day").format("MM/DD/YYYY");
-        
+
         $("#day-1").text(date1);
         $("#day-2").text(date2);
         $("#day-3").text(date3);
@@ -97,7 +90,6 @@ $("#search-button").on("click", function (event) {
             method: "GET"
         })
             .then(function (response) {
-                console.log(response);
 
                 var icon1Code = response.list[7].weather[0].icon;
                 var icon2Code = response.list[15].weather[0].icon;
